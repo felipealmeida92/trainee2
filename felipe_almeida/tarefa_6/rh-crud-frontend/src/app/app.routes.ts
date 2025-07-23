56f70b64d6b9db757c1482432c91cc635e3d55f6
felipealmeida92 <you@example.com>
2025-07-23 09:50:04 -0300

    refactor: move requests module to features/requests and update imports

--- felipe_almeida/tarefa_6/rh-crud-frontend/src/app/app.routes.ts
+++ felipe_almeida/tarefa_6/rh-crud-frontend/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { RequestListComponent } from './requests/pages/request-list/request-list.component';
-import { RequestFormComponent } from './requests/pages/request-form/request-form.component';
+import { RequestListComponent } from './features/requests/pages/request-list/request-list.component';
+import { RequestFormComponent } from './features/requests/pages/request-form/request-form.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'requests', pathMatch: 'full' },
